e2eaee030128fa403b938dae2a3ba7f79a2d7a72
lin-cr8 <133610685+lin-cr8@users.noreply.github.com>
2025-10-15 16:17:10 +0200

    Add files via upload
    2 Testfunktionen fürs LED testen, aufräumen, header file

--- include/ledStrip.h
+++ include/ledStrip.h
@@ -1,8 +1,8 @@
 #ifndef LEDSTRIP_H
 #define LEDSTRIP_H
+
 #include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
-
 #include "peakDetectorState.h"
 
 #define LED_BUILTIN 2
@@ -10,7 +10,11 @@
 #define LED_COUNT 8
 
 extern Adafruit_NeoPixel strip;
+
 void activateLEDsOnce(uint8_t reset_pos);
 void controlLEDs(PeakDetectorState *detector);
+
+void testStripSetup();
+void testStripLoop(PeakDetectorState *detector);
 
 #endif
